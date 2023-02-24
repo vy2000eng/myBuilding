eb3bc7c64e639e2fcc079ee037466476a48b9634
vy2000eng <vy2000eng@gmail.com>
2023-02-24 11:21:55 -0500

    added some stuff to listings

--- front_end/src/components/Listings.jsx
+++ front_end/src/components/Listings.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import {auth} from "../firebase-config"
 import { useAuthState } from "react-firebase-hooks/auth";
+import { SingleListing } from './SingleListing';
 
 export const Listings = () => {
 
@@ -13,6 +14,19 @@ export const Listings = () => {
   
   
   return (
-    <div>Listings</div>
+    <div className="flex flex-col gap-2 p-2 ">
+
+    <SingleListing/>
+    <SingleListing/>
+    <SingleListing/>
+
+    <SingleListing/>
+      <SingleListing/>
+      <SingleListing/>
+
+      <SingleListing/>
+
+
+  </div>
   )
 }
